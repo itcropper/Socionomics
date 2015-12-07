80ac27e442073ca36dcdb858389d754edde8f557
Ian <itcropper@gmail.com>
2015-12-07 13:52:48 -0700

    changed limit amount

--- Controllers/TwitterMarker.js
+++ Controllers/TwitterMarker.js
@@ -19,8 +19,8 @@ exports.getAll = function(cb){
     
     Marker
         .find({"count" : { $exists : true }})
-        .sort({ time : -1 })
-        .limit(4000)
+        .sort({ time : 1 })
+        .limit(5000)
         .exec(function(err, res){
             if(!err){ 
                 cb(res); 

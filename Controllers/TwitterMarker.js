bbfcb8bc346a33eb346c7c3659e92c9c23ff5854
Ian <itcropper@gmail.com>
2016-06-13 09:39:10 -0600

    increased range of data points

--- Controllers/TwitterMarker.js
+++ Controllers/TwitterMarker.js
@@ -20,7 +20,7 @@ exports.getAll = function(cb){
     Marker
         .find({"count" : { $exists : true }})
         .sort({time : 1})
-        .limit(3500)
+        .limit(7500)
         .sort({ time : -1 })
         .exec(function(err, res){
             if(!err){ 
